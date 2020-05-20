e069b14da476f405684e6fe995e8fcefaa73c391
Jack Davis <jbad90@gmail.com>
2020-05-20 09:22:05 +0100

    Add untested bootstrap script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,30 @@
 ROOT=~/Git/dotfiles
 
-mkdir ~/.vim
+function setup_vim {
+  mkdir ~/.vim
+  
+  ln -s $ROOT/config .config
+  ln -s $ROOT/vim/.vimrc .vimrc
+  ln -s $ROOT/vim/UltiSnips ~/.vim/UltiSnips
+}
 
-ln -s $ROOT/config .config
-ln -s $ROOT/vim/.vimrc .vimrc
-ln -s $ROOT/vim/UltiSnips ~/.vim/UltiSnips
+function setup_zsh {
+  echo "source ~/Git/dotfiles/zsh/zsh.sh" >> ~/.zshrc
+}
+
+function setup_brew {
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  
+  brew install fd hub ripgrep
+  brew install vim --HEAD
+}
+
+function setup {
+  setup_vim
+  setup_zsh
+  setup_brew
+}
+
+setup
+
 
